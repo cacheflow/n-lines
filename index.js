96ed44d8668b762c39f080476b4b4b12caaef039
lex alexander <l.alexander10@gmail.com>
2017-06-12 12:42:27 -0700

    Added function to check for errors.

--- index.js
+++ index.js
@@ -2,22 +2,13 @@
 
 module.exports = ((numOfNewLines = 1, opts = {}, ...args) => {
   let numOfNewLinesAsInt = parseInt(numOfNewLines)
-  if ( isNaN(numOfNewLines) ) {
-    throw new Error("Looks like you didn't pass a number as your first argument. Try passing a number like 1.")
-  }
-  if ( !args.length ) {
-    throw new Error("Looks like you didn't pass a string to add lines to. Try passing a proper string.")
-  }
-  if ( Object.create(opts).constructor != Object) {
-    throw new Error( 'Looks like options is not an object. Try passing an object of your options like {prepend: true}' )
-  }
-  let optsKeys = Object.keys(opts)
+  ensureNoErrors(numOfNewLines, args, opts)
   let obj = Object.assign(opts)
   let newArr = []
   obj['append'] === true ? newArr = newArr.concat(...args) : newArr.push(...args)
   newArr = newArr.filter(el => el != '\n')
   for(var i = 0; i < numOfNewLinesAsInt; i+=1) {
-    if(optsKeys.length > 0) {
+    if(Object.keys(opts).length > 0) {
       for(var key in obj) {
         if ( (key === 'prepend' && obj[key] === true) ) {
           newArr.unshift("\n")
@@ -36,3 +27,16 @@ module.exports = ((numOfNewLines = 1, opts = {}, ...args) => {
   }
   return newArr.join('\n')
 })
+
+
+const ensureNoErrors = (numOfNewLines, args, opts) => {
+  if ( isNaN(numOfNewLines) ) {
+    throw new Error("Looks like you didn't pass a number as your first argument. Try passing a number like 1.")
+  }
+  if ( !args.length ) {
+    throw new Error("Looks like you didn't pass a string to add lines to. Try passing a proper string.")
+  }
+  if ( Object.create(opts).constructor != Object) {
+    throw new Error( 'Looks like options is not an object. Try passing an object of your options like {prepend: true}' )
+  }
+}
